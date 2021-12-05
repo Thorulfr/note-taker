8b98fbc48657345a84046616b7adcaf3e1d29f0c
Ben H <deepfreespace@gmail.com>
2021-12-04 20:38:29 -0700

    Renamed db to Data

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -2,4 +2,9 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db.json');
 
+// Route for getting notes
+router.get('/', (req, res) => {
+    let results = notes;
+});
+
 module.exports = router;
