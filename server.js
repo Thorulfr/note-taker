773ec78c8bb1e8fe90658eed996efcca957fe9b5
Ben H <deepfreespace@gmail.com>
2021-12-04 18:47:23 -0700

    Server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+// Imports
+const express = require('express');
+const routes = require('./routes');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// Server bits and bobs
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Enable and prefix routes
+app.use(routes);
+app.use('./api', apiRoutes);
+app.use('./', htmlRoutes);
+
+// Boot server
+app.listen(PORT, () => {
+    console.log(`Server now running on port ${PORT}`);
+});
