f2417a9a444a6c62b7eba9fb8a5056597a147cda
Ben H <deepfreespace@gmail.com>
2021-12-05 17:27:57 -0700

    Get and Post routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 const notesRoutes = require('./notesRoutes');
 
 // Prepend 'notes' to notes routes
-router.use('./notes', notesRoutes);
+router.use('/notes', notesRoutes);
 
 module.exports = router;
--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,10 +1,21 @@
 // Imports
 const router = require('express').Router();
-const { notes } = require('../../db/db.json');
+const notes = require('../../data/db.json');
+const { createNewNote } = require('../../lib/notes');
 
-// Route for getting notes
+// Get all notes
 router.get('/', (req, res) => {
     let results = notes;
+    res.json(results);
+});
+
+// Add new note
+router.post('/', (req, res) => {
+    // Set ID based on what next index of the array will bear
+    req.body.id = notes.length.toString();
+    // Add note to JSON file
+    const note = createNewNote(req.body, notes);
+    res.json(note);
 });
 
 module.exports = router;
