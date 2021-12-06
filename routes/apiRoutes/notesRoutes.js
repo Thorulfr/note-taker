e8acac858208bbeaebbf8edc4b6ae2c1b3912eba
Ben H <deepfreespace@gmail.com>
2021-12-05 22:39:38 -0700

    Fixed stupid note-deleting bug

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,6 +1,7 @@
 // Imports
 const router = require('express').Router();
 const notes = require('../../data/db');
+const { nanoid } = require('nanoid');
 const { createNewNote, deleteNote } = require('../../lib/notes');
 
 // Get all notes
@@ -12,7 +13,7 @@ router.get('/', (req, res) => {
 // Add new note
 router.post('/', (req, res) => {
     // Set ID based on what next index of the array will bear
-    req.body.id = notes.length.toString();
+    req.body.id = nanoid();
     // Add note to JSON file
     const note = createNewNote(req.body, notes);
     res.json(note);
